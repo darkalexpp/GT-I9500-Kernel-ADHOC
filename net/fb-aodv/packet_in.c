e4389b537878a730239ed1f67497f16c06235ab8
darkalexpp <darkalexpp@gmail.com>
2018-02-14 21:57:06 -0500

    Modulo fb-aodv

---
+++ net/fb-aodv/packet_in.c
@@ -0,0 +1,264 @@
+/***************************************************************************
+ packet_in.c  -  description
+ -------------------
+ begin                : Mon Jul 29 2002
+ copyright            : (C) 2002 by Luke Klein-Berndt
+ email                : kleinb@nist.gov
+ ***************************************************************************/
+/***************************************************************************
+ Modified by Miguel Catalan Cid - miguel.catcid@gmail.com - Version: Mon Jan 1 2010
+ ***************************************************************************/
+
+#include "packet_in.h"
+
+#ifdef DTN
+	extern int dtn_register;
+#endif
+#ifdef BLACKLIST
+	extern u_int32_t aodv_blacklist_ip[10];
+	extern u_int32_t dtn_blacklist_ip[10];
+	extern int aodv_blacksize;
+	extern int dtn_blacksize;
+#endif
+
+int valid_aodv_packet(int numbytes, int type, char *data, struct timeval tv) {
+
+	rerr *tmp_rerr;
+
+	rrep *tmp_rrep;
+	hello *tmp_hello;
+	rreq *tmp_rreq;
+	rreq_st *tmp_strreq;
+
+#ifdef RECOVERYPATH
+	rcvp *tmp_rcvp;
+	rrdp *tmp_rrdp;
+#endif
+
+	s_probe *tmp_sprobe;
+	l_probe *tmp_lprobe;
+	ett_info *tmp_ett_info;
+
+	switch (type) {
+
+	case ST_RREQ_MESSAGE:
+		tmp_strreq = (rreq_st *) data;
+		if (numbytes == sizeof(rreq_st)) {
+			return 1;
+		}
+		break;
+
+	case RREP_MESSAGE:
+
+		tmp_rrep = (rrep *) data;
+		if (numbytes == sizeof(rrep)) {
+			return 1;
+		}
+		break;
+
+	case RERR_MESSAGE:
+		tmp_rerr = (rerr *) data;
+		if (numbytes == sizeof(rerr)) {
+			return 1;
+		}
+		break;
+
+	case HELLO_MESSAGE:
+		tmp_hello = (hello *) data;
+		if (numbytes == (sizeof(hello) + (sizeof(hello_extension)
+				* tmp_hello->neigh_count))) {
+			return 1;
+		}
+		break;
+
+    /****************Ìí¼ÓÀàÐÍRCVP_MESSAGE*******************/
+	#ifdef RECOVERYPATH
+    	case RCVP_MESSAGE:
+        	tmp_rcvp = (rcvp *) data;
+        	if(numbytes == sizeof(rcvp))
+       			return 1;
+        	break;
+	case RRDP_MESSAGE:
+		tmp_rrdp = (rrdp *) data;
+        	if(numbytes == sizeof(rrdp))
+       			return 1;
+        	break;
+	#endif
+
+	case ETT_S_MESSAGE: //ETT small message received
+		tmp_sprobe = (s_probe *) data;
+		if (numbytes == sizeof(s_probe)) {
+			tmp_sprobe->sec = (u_int32_t) tv.tv_sec;
+			tmp_sprobe->usec= (u_int32_t) tv.tv_usec;
+			return 1;
+		}
+		break;
+
+	case ETT_L_MESSAGE: //ETT large message received
+		tmp_lprobe = (l_probe *) data;
+		if (numbytes == sizeof(l_probe)) {
+			tmp_lprobe->sec = (u_int32_t) tv.tv_sec;
+			tmp_lprobe->usec= (u_int32_t) tv.tv_usec;
+			return 1;
+		}
+		break;
+
+	case RREQ_MESSAGE:
+		tmp_rreq = (rreq *) data;
+		if (numbytes == sizeof(rreq))
+			return 1;
+		break;
+
+
+	case ETT_INFO_MSG:
+		tmp_ett_info = (ett_info *) data;
+		if (numbytes == sizeof(ett_info))
+			return 1;
+		break;
+
+	default:
+		break;
+
+	}
+	return 0;
+}
+
+int packet_in(struct sk_buff *packet, struct timeval tv) {
+	struct iphdr *ip;
+
+	// Create aodv message types
+	u_int8_t aodv_type;
+
+	//The packets which come in still have their headers from the IP and UDP
+	int start_point = sizeof(struct udphdr) + sizeof(struct iphdr);
+
+	//get pointers to the important parts of the message
+	ip = ip_hdr(packet);
+
+	aodv_type = (int) packet->data[start_point];
+#ifdef CaiDebug
+    if( aodv_type == RCVP_MESSAGE)
+        printk("It's a rcvp message\n");
+#endif
+#ifdef DEBUG
+	if ( aodv_type != HELLO_MESSAGE )
+		printk("packet_in: type: %d and of size %u from: %s\n", aodv_type, packet->len - start_point, inet_ntoa(ip->saddr));
+#endif
+
+	if (!valid_aodv_packet(packet->len - start_point, aodv_type, packet->data
+			+ start_point, tv)) {
+
+#ifdef DEBUG
+		printk("Packet of type: %d and of size %u from: %s failed packet check!\n", aodv_type, packet->len - start_point, inet_ntoa(ip->saddr));
+#endif
+		return NF_DROP;
+
+	}
+
+	insert_task(aodv_type, packet);
+
+	return NF_ACCEPT;
+}
+
+extern int initialized;
+unsigned int input_handler(unsigned int hooknum, struct sk_buff *skb,
+		const struct net_device *in, const struct net_device *out, int (*okfn) (struct sk_buff *)) {
+	struct timeval tv;
+
+	u_int8_t aodv_type;
+	int start_point = sizeof(struct udphdr) + sizeof(struct iphdr);
+	aodv_type = (int) skb->data[start_point];
+
+	struct iphdr *ip = ip_hdr(skb);
+	struct in_device *tmp_indev = (struct in_device *) in->ip_ptr;
+
+	void *p = (uint32_t *) ip + ip->ihl;
+	struct udphdr *udp = (struct udphdr *) p;
+
+	if (!initialized) { // this is required otherwise kernel calls this function without insmod completing the module loading process.
+		return NF_ACCEPT;
+	}
+
+
+	char src[16];
+	char dst[16];
+	strcpy(src, inet_ntoa(ip->saddr));
+	strcpy(dst, inet_ntoa(ip->daddr));
+ 
+
+#ifdef DTN
+	static int t = 1;
+	//DTN register
+	if (t && !dtn_register && udp->dest == htons(DTNREGISTER)){
+		dtn_register = 1;
+		t = 0;
+		printk("*************DTN registered*************\n");
+	}
+	
+	extern u_int32_t g_mesh_ip;	
+	if( dtn_register && udp->dest == htons(GET_DTN_HELLO) ){
+		insert_timer_simple(TASK_DTN_HELLO, 0, g_mesh_ip);
+		update_timer_queue();
+		printk("*************GOT DTN HELLO TASK*************\n");
+	}
+
+	if( dtn_register && udp->dest == htons(AODV_LOCATION_PORT)){
+		extern u_int32_t dtn_hello_ip;
+		extern u_int32_t longitude;
+		extern u_int32_t latitude;
+		void *loc_data;
+		loc_data = &(skb->data[start_point]);
+		u_int32_t src_ip = ((u_int32_t *)loc_data)[0];
+		u_int32_t tos = ((u_int32_t *)loc_data)[1];
+		u_int32_t x = ((u_int32_t *)loc_data)[2];
+		u_int32_t y = ((u_int32_t *)loc_data)[3];
+		longitude = x;
+		latitude = y;
+		/*u_int32_t src_ip = ((u_int32_t *)skb->data)[0];
+		u_int32_t tos = ((u_int32_t *)skb->data)[1];
+		u_int32_t x = ((u_int32_t *)skb->data)[2];
+		u_int32_t y = ((u_int32_t *)skb->data)[3];*/
+#ifdef CaiDebug
+		printk("*********GOT Location Info%s  %ld  %ld**********\n",inet_ntoa(src_ip),ntohl(x),ntohl(y));	
+#endif
+		//gen_rrep(src_ip,dtn_hello_ip,(unsigned char)tos);
+		insert_timer_directional(TASK_SEND_RREP, RREP_TIMER, 0,
+						src_ip, dtn_hello_ip,(unsigned char)tos);
+		update_timer_queue();
+}
+#ifdef BLACKLIST
+	int k = 0;
+	if (udp->dest == htons(9556)) {
+		for(k=0; k<dtn_blacksize; k++) {
+			if (!strcmp("192.168.1.255",dst) && dtn_blacklist_ip[k] == ip->saddr) {
+				return NF_DROP;
+			}
+		}
+	}
+#endif
+
+#endif
+
+#ifdef DEBUG
+	if (ip->protocol == IPPROTO_ICMP) {
+		printk("input_handler: Recv a ICMP from %s to %s\n", src, dst);
+	} else if (aodv_type != HELLO_MESSAGE) {
+		printk("input_handler: Recv a %d Packet, from %s to %s\n",ip->protocol, src, dst);
+	}
+#endif
+	if (udp != NULL && ip->protocol == IPPROTO_UDP && skb->protocol == htons(ETH_P_IP) && udp->dest == htons(AODVPORT) && tmp_indev->ifa_list->ifa_address != ip->saddr) {
+
+		do_gettimeofday(&tv); //MCC - capture the time of arrival needed for ett_probes
+		return packet_in((skb), tv);
+
+	}
+
+#ifdef DEBUG0
+	if (aodv_type != HELLO_MESSAGE)
+		printk("input_handler: input without process.\n");
+#endif
+
+	return NF_ACCEPT;
+
+}
+
---
+++ net/fb-aodv/rerr.c
@@ -0,0 +1,406 @@
+/***************************************************************************
+ rerr.c  -  description
+ -------------------
+ begin                : Mon Aug 11 2003
+ copyright            : (C) 2003 by Luke Klein-Berndt
+ email                : kleinb@nist.gov
+ ***************************************************************************/
+/***************************************************************************
+ Modified by Miguel Catalan Cid - miguel.catcid@gmail.com - Version: Mon Jan 1 2010
+ ***************************************************************************/
+#include "rerr.h"
+
+extern u_int32_t g_mesh_ip;
+extern u_int32_t g_null_ip;
+extern u_int8_t g_aodv_gateway;
+extern u_int8_t g_routing_metric;
+
+
+int reply_to_rrer(u_int32_t source, u_int32_t destination) {
+
+
+	if (source == g_mesh_ip || (source == g_null_ip && g_aodv_gateway))
+		return 1;
+
+	return 0;
+}
+
+//brk_dst_ipΪ�ѶϿ���·��Ŀ��ip
+int gen_rerr(u_int32_t brk_dst_ip) {
+	aodv_route *tmp_route;
+	rerr *tmp_rerr;
+	int expired_routes = 0;
+
+	//�ҵ���һ��aodv·��
+	tmp_route = first_aodv_route();
+
+	//go through list
+	//����������һ��Ϊbrk_dst_ip��·����Ŀ�������ýڵ㷢��rerr����Ч����·��
+	while (tmp_route != NULL) {
+
+		//printk("tmp_route->next_hop is %s\n",inet_ntoa(tmp_route->next_hop));
+		if ((tmp_route->next_hop == brk_dst_ip) && (!tmp_route->self_route)&& (!tmp_route->neigh_route)){
+
+			if (!reply_to_rrer(tmp_route->src_ip, tmp_route->dst_ip)) { //i'm source of the route, don't send the rerr
+				if (tmp_route->state != INVALID) {
+
+					if ((tmp_rerr = (rerr *) kmalloc(sizeof(rerr), GFP_ATOMIC))
+							== NULL) {
+#ifdef DEBUG
+						printk("Can't allocate new rrep\n");
+#endif
+						return 0;
+					}
+
+					tmp_rerr->type = RERR_MESSAGE;
+					tmp_rerr->dst_count = 0; //unused
+					tmp_rerr->reserved = 0;
+					tmp_rerr->n = 0;
+					tmp_rerr->num_hops = 0;
+					tmp_rerr->dst_ip = tmp_route->dst_ip;
+					tmp_rerr->dst_id = htonl(tmp_route->dst_id);
+
+#ifdef DTN
+					//if there is DTN register,current node will be the
+					//last DTN hop near the brk node
+					extern int dtn_register;
+					tmp_rerr->last_avail_ip = NULL;
+
+					tmp_rerr->src_ip = tmp_route->src_ip;
+					if(dtn_register==1)
+					{
+						tmp_rerr->last_avail_ip = g_mesh_ip;
+			        //printk("last_avail_ip:%s\n",inet_ntoa(tmp_rerr->last_avail_ip));
+						//notice DTN layer
+#include <linux/sched.h>
+//JL: Added kernel threads interface:
+#include <linux/kthread.h>
+						u_int32_t para[4];
+						para[0] = tmp_rerr->src_ip;
+						para[1] = tmp_rerr->dst_ip;
+						para[2] = tmp_rerr->last_avail_ip;
+
+						//���ʹ���
+						para[3] = (u_int32_t)tmp_rerr->type;
+						//
+						send2dtn((void*)para,DTNPORT);
+
+					}
+					else tmp_rerr->last_avail_ip = NULL;
+#endif
+
+#ifdef CaiDebug
+					//cai:test gen rerr
+	char local[20];
+ 	strcpy(local,inet_ntoa(g_mesh_ip));
+	char brk[20];
+strcpy(brk,inet_ntoa(brk_dst_ip));
+	char last[20];
+strcpy(last,inet_ntoa(tmp_route->last_hop));
+
+	printk("rerr src is %s,brk_dst_ip is %s,the last hop is %s\n",local,brk,last);
+	
+#endif
+                    		send_message(tmp_route->last_hop, NET_DIAMETER, tmp_rerr,sizeof(rerr));
+                     kfree(tmp_rerr);
+
+#ifdef RECOVERYPATH
+                    /****************************************************
+                        ������·��������ÿ������һ���µ�rerr��ʱ��Ҳ
+                        �½�һ��brk_list����Ŀ
+                    *****************************************************/
+
+                    brk_link *tmp_link;
+                    u_int32_t lastavail=NULL;
+#ifdef DTN
+                    if(dtn_register==1)
+                    {
+                            lastavail = g_mesh_ip;
+                    }
+#endif
+                    tmp_link = find_brk_link(tmp_route->src_ip,tmp_route->dst_ip);
+                    if(tmp_link != NULL){//�Ѵ��ڣ������������ڣ������������ٳ���
+                        //Update brk_link timelife������Ҫ��ʱ���Ĵ���
+                        tmp_link->lifetime = getcurrtime() + BRK_LINK_TIMEOUT;
+                        flush_brk_list();
+
+                    }
+                    else{
+                        tmp_link = create_brk_link(tmp_route->src_ip,tmp_route->dst_ip,
+                                               tmp_route->last_hop,lastavail);//����������
+                        if(tmp_link==NULL) printk("Creat new brk link failed!\n");
+#ifdef CaiDebug
+                        char s[20];
+                        char d[20];
+                        char l[20];
+                        char la[20];
+                        strcpy(s,inet_ntoa(tmp_route->src_ip));
+                        strcpy(d,inet_ntoa(tmp_route->dst_ip));
+                        strcpy(l,inet_ntoa(tmp_route->last_hop));
+                        strcpy(la,inet_ntoa(lastavail));
+                        printk("new brk link %s:%s:%s:%s\n",s,d,l,la);
+#endif
+                        flush_brk_list();
+                    }
+                    /******************************************************************/
+#endif
+
+
+				}//route is not invalid
+
+
+			}//not the source of the route
+#ifdef RECOVERYPATH
+			else{		    
+				/****************************************************         					*****************************************************/
+#ifdef DTN
+					//if there is DTN register,current node will be the
+					//last DTN hop near the brk node
+					extern int dtn_register;
+					u_int32_t last_avail;//put them out of #ifdef DTN or in is a Q 
+					last_avail = NULL;
+					if(dtn_register==1)
+					{
+						last_avail = g_mesh_ip;
+						//notice DTN layer
+#include <linux/sched.h>
+//JL: Added kernel threads interface:
+#include <linux/kthread.h>
+						u_int32_t para[4];
+						para[0] = tmp_route->src_ip;
+						para[1] = tmp_route->dst_ip;
+						para[2] = last_avail;
+
+						//���ʹ���
+						para[3] = (u_int32_t)RERR_MESSAGE;
+						//
+						send2dtn((void*)para,DTNPORT);
+
+					}
+#endif
+				brk_link *tmp_link;
+				tmp_link = find_brk_link(tmp_route->src_ip,tmp_route->dst_ip);
+				if(tmp_link != NULL){
+					tmp_link->lifetime = getcurrtime() + BRK_LINK_TIMEOUT;
+					flush_brk_list();                    
+				}
+				else{
+				tmp_link = create_brk_link(tmp_route->src_ip,tmp_route->dst_ip,
+								NULL,last_avail);//notice the last hop is NULL
+				if(tmp_link==NULL) printk("Creat new brk link failed!\n");
+#ifdef CaiDebug
+char s[20];
+char d[20];
+char l[20];
+char la[20];
+strcpy(s,inet_ntoa(tmp_route->src_ip));
+strcpy(d,inet_ntoa(tmp_route->dst_ip));
+strcpy(l,"0.0.0.0");
+strcpy(la,inet_ntoa(last_avail));
+printk("new brk link %s:%s:%s:%s\n",s,d,l,la);
+#endif
+				flush_brk_list();
+				}                    
+				/******************************************************************/
+			}//the source
+#endif
+
+			if (tmp_route->state != INVALID){
+				expire_aodv_route(tmp_route);
+			expired_routes++;
+#ifdef CaiDebug
+	char local[20];
+ 	strcpy(local,inet_ntoa(g_mesh_ip));
+	char last[20];
+	strcpy(last,inet_ntoa(tmp_route->last_hop));
+	printk("invalidate tmp_route %s to %s,expored_route is %d\n",local,last,expired_routes);
+#endif
+			}
+		}
+		//move on to the next entry
+		tmp_route = tmp_route->next;
+
+	}
+	if (g_routing_metric == WCIM && expired_routes != 0)
+
+	update_my_load();
+
+
+	return 0;
+}
+
+
+int recv_rerr(task * tmp_packet) {
+	aodv_route *tmp_route;
+	rerr *tmp_rerr;
+	int num_hops;
+	int expired_routes = 0;
+
+	tmp_rerr = (rerr *) tmp_packet->data;
+	num_hops = tmp_rerr->num_hops+1;
+
+#ifdef CaiDebug
+	printk("Recieved a route error from %s\n", inet_ntoa(tmp_packet->src_ip));
+#endif
+
+#ifdef DTN
+
+	extern int dtn_register;
+	int first_dtn=0;
+
+#include <linux/sched.h>
+//JL: Added kernel threads interface:
+#include <linux/kthread.h>
+	u_int32_t para[4];
+	para[0] = tmp_rerr->src_ip;
+	para[1] = tmp_rerr->dst_ip;
+	para[2] = tmp_rerr->last_avail_ip;
+	para[3] = (u_int32_t)tmp_rerr->type;
+
+	if( dtn_register==1 ){
+	    //����ע��DTN����lastavailΪ�ջ��߽��յ���tmp_rerr��СС�ڶ�����DTN��rerr
+		if( para[2]==NULL )//current node is the nearest DTN node to brk link
+		{
+			para[2] = g_mesh_ip;
+			first_dtn = 1;
+		}
+//		kthread_run(&send2dtn, (void*)para, "send2dtn");
+		send2dtn((void*)para,DTNPORT);//tmp_rerr->dst_ip, tmp_rerr->last_avail_ip);
+	}
+
+#endif
+
+    tmp_route
+			= find_aodv_route_by_id(tmp_rerr->dst_ip, ntohl(tmp_rerr->dst_id));
+
+	if (tmp_route && tmp_route->next_hop == tmp_packet->src_ip) {
+		//if there is any hop before me
+
+		if (!reply_to_rrer(tmp_route->src_ip, tmp_route->dst_ip)) {
+
+			if (tmp_route->state != INVALID) { //route with active traffic
+
+				tmp_rerr->type = RERR_MESSAGE;
+				tmp_rerr->dst_count = 0; //unused
+				tmp_rerr->reserved = 0;
+				tmp_rerr->n = 0;
+				tmp_rerr->num_hops = num_hops;
+				tmp_rerr->dst_ip = tmp_route->dst_ip;
+				tmp_rerr->dst_id = htonl(tmp_route->dst_id);
+
+#ifdef DTN
+				tmp_rerr->src_ip = tmp_route->src_ip;
+
+				if(dtn_register==1 && first_dtn==1)
+				{
+					tmp_rerr->last_avail_ip = g_mesh_ip;
+				}
+#endif
+
+
+				send_message(tmp_route->last_hop, NET_DIAMETER, tmp_rerr,
+						sizeof(rerr));
+
+#ifdef RECOVERYPATH
+                    /****************************************************
+                        ������·��������ÿ������һ���µ�rerr��ʱ��Ҳ
+                        �½�һ��brk_list����Ŀ
+                    *****************************************************/
+                    brk_link *tmp_link;
+
+                    tmp_link = find_brk_link(tmp_route->src_ip,tmp_route->dst_ip);
+                    if(tmp_link != NULL){//�Ѵ��ڣ������������ڣ������������ٳ���
+                        //Update brk_link timelife������Ҫ��ʱ���Ĵ���
+                        tmp_link->lifetime = getcurrtime() + BRK_LINK_TIMEOUT;
+                        flush_brk_list();
+
+                    }
+                    else{
+                        tmp_link = create_brk_link(tmp_route->src_ip,tmp_route->dst_ip,
+                                               tmp_route->last_hop,tmp_rerr->last_avail_ip);//����������,ע���˴���ast_avail_ipӦ���Զ�·��
+
+                        if(tmp_link==NULL) printk("Creat new brk link failed!\n");
+#ifdef CaiDebug
+                        char s[20];
+                        char d[20];
+                        char l[20];
+                        char la[20];
+                        strcpy(s,inet_ntoa(tmp_route->src_ip));
+                        strcpy(d,inet_ntoa(tmp_route->dst_ip));
+                        strcpy(l,inet_ntoa(tmp_route->last_hop));
+                        strcpy(la,inet_ntoa(tmp_rerr->last_avail_ip));
+                        printk("new brk link %s:%s:%s:%s\n",s,d,l,la);
+#endif
+                        flush_brk_list();
+                    }
+                    /******************************************************************/
+#endif
+
+
+
+
+			}
+
+		}//not the source
+#ifdef RECOVERYPATH
+		else{
+		    /****************************************************
+                        ������·��������ÿ������һ���µ�rerr��ʱ��Ҳ
+                        �½�һ��brk_list����Ŀ
+
+                    *****************************************************/
+#ifdef DTN
+		    
+			u_int32_t last_avail;
+			last_avail = NULL;
+			if(dtn_register==1 && first_dtn==1)
+			{	
+				last_avail = g_mesh_ip;
+			}
+#endif
+                    brk_link *tmp_link;
+
+                    tmp_link = find_brk_link(tmp_route->src_ip,tmp_route->dst_ip);
+                    if(tmp_link != NULL){//�Ѵ��ڣ������������ڣ������������ٳ���
+                        //Update brk_link timelife������Ҫ��ʱ���Ĵ���
+                        tmp_link->lifetime = getcurrtime() + BRK_LINK_TIMEOUT;
+                        flush_brk_list();
+
+                    }
+                    else{
+                        tmp_link = create_brk_link(tmp_route->src_ip,tmp_route->dst_ip,
+                                               NULL,last_avail);//notice the last hop is NULL
+
+                        if(tmp_link==NULL) printk("Creat new brk link failed!\n");
+#ifdef CaiDebug
+                        char s[20];
+                        char d[20];
+                        char l[20];
+                        char la[20];
+                        strcpy(s,inet_ntoa(tmp_route->src_ip));
+                        strcpy(d,inet_ntoa(tmp_route->dst_ip));
+                        strcpy(l,"0.0.0.0");
+                        strcpy(la,inet_ntoa(last_avail));
+                        printk("new brk link %s:%s:%s:%s\n",s,d,l,la);
+
+#endif
+                        flush_brk_list();
+                    }
+                    /******************************************************************/
+		}//the source
+#endif
+
+		if (tmp_route->state != INVALID) {
+			expire_aodv_route(tmp_route);
+			expired_routes++;
+		}
+
+	}
+	if (g_routing_metric == WCIM && expired_routes != 0)
+	update_my_load();
+
+
+	return 0;
+
+}
+
